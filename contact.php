7f8d64a08a2493256487383b836cdb404d224f11
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2021-06-24 06:25:26 +0900

    contactページheader,footer分割

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php get_header("page"); ?>
+
+    <main>
+      <section>
+        <div class="contact-container">
+          <div class="contact-heading">
+            <h1>Contact</h1>
+          </div>
+          <div class="contact-message">
+            <p>魚の注文から、有田鮮魚に関するお問い合わせはこちらのフォームからお願いいたします。</p>
+          </div>
+          <div class="contact-form">
+            <form action="" method="POST">
+              <div>
+                <label for="name">お名前</label><br>
+                <input type="text" id="name" name="your-name" placeholder="有田　魚助">
+              </div>
+              <div>
+                <label for="e-mail">メールアドレス</label><br>
+                <input type="text" id="e-mail" name="e-mail" placeholder="1234.5678@gmail.com">
+              </div>
+              <div>
+                <label for="content">お問い合わせ内容</label><br>
+                <textarea id="contact" name="contact" placeholder="お問い合わせ内容"></textarea>
+              </div>
+            </div>
+              <div>
+                <input type="submit" id="submit" value="送信する">
+              </div>
+          </form>
+        </div>
+
+      </section>
+    </main>
+
+<?php get_footer(); ?>
