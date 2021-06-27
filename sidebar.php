3c24e2ec889d703f55853fa92afe9d3e6a0ab533
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2021-06-27 09:16:30 +0900

    さいどばーの作成

---
+++ sidebar.php
@@ -0,0 +1,10 @@
+<aside class="category">
+  <h2 class="category-title">カテゴリー一覧</h2>
+  <ul class="category-list">
+    <?php
+    $args = array(
+      "title_li"=>'',
+    );
+    wp_list_categories($args);
+    ?>
+</aside>
